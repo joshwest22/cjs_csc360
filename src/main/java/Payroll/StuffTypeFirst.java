8bb97e07517a89bda8cfb831d015c593cd97e5a4
Josh West <josh.west@centre.edu>
2022-02-14 16:55:51 -0500

    added switch statement that does nothing

--- src/main/java/Payroll/StuffTypeFirst.java
+++ src/main/java/Payroll/StuffTypeFirst.java
@@ -5,6 +5,18 @@ public class StuffTypeFirst implements StuffType
 
 	public double getBReaD(int sourdough, int yeast)
 	{
+		int expression = 0;
+		switch(expression) {
+		  case 1:
+		    System.out.println("It's so beautiful outside...");
+		    break;
+		  case 42:
+			  System.out.println("I know kung fu");
+		    break;
+		  default:
+		    expression = 0;
+		}
+
 		/*The Ultimate Answer to Life, The Universe and Everything is...42! */
 		return sourdough * yeast;
 	}
